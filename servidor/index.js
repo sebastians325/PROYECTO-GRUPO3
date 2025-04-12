762f8175f99629c133cc8bcc786ccb3fde0c4603
sebasge15 <sebasge150401@gmail.com>
2025-04-12 18:40:52 -0500

    frontend basico y be cors

--- servidor/index.js
+++ servidor/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const db = require('./models')
 app.use(express.json())
+app.use(cors())
 
 //Rutas
 const postRouter = require('./routes/usuarios')
