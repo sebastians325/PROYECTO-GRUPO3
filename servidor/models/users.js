4cddc37ffa241cb81877ec4706dec36ec7de7dbe
sebasge15 <99986398+sebasge15@users.noreply.github.com>
2025-04-18 18:36:52 -0500

    Revert "Feat(user.js): added password and types of users, freelancer and cliente"
    This reverts commit 0801c1c145a5ad5148c241a1168fa16694299347.

--- servidor/models/users.js
+++ servidor/models/users.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         role : {
-            type: DataTypes.ENUM('freelancer', 'cliente'),
+            type: DataTypes.ENUM('frelancer', 'cliente'),
             allowNull: false
         }
     }, {
