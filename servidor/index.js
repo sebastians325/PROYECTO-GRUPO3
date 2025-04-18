8deb890c50c1f3b099b5769776ad857d50a673a7
sebasge15 <99986398+sebasge15@users.noreply.github.com>
2025-04-18 18:38:26 -0500

    Revert "frontend basico y be cors"
    This reverts commit 762f8175f99629c133cc8bcc786ccb3fde0c4603.

--- servidor/index.js
+++ servidor/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 const db = require('./models')
 app.use(express.json())
-app.use(cors())
 
 //Rutas
 const postRouter = require('./routes/usuarios')
