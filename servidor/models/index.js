6bf4b74acd0364c9877f18c9c905f116f621b947
sebasge15 <sebasge150401@gmail.com>
2025-05-18 02:36:29 -0500

    fixed

--- servidor/models/index.js
+++ servidor/models/index.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const db = require('./models'); 
-const app = express();
-const PORT = process.env.PORT || 3001;
+'use strict';
 
-app.use(express.json());
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-app.use('/api', require('./routes/rutaEjemplo'));
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-const initializeDatabase = async () => {
-  try {
-
-    await db.sequelize.authenticate();
-    console.log('Conexión a la base de datos establecida.');
-
-    // Sincronizar modelos
-    await db.sequelize.sync({
-      force: false,   
-      alter: process.env.NODE_ENV === 'development' 
-    });
-    
-    console.log('Modelos sincronizados con la base de datos.');
-  } catch (error) {
-    console.error('Error al inicializar la base de datos:', error);
-    process.exit(1);
-  }
-};
-
-// Iniciar servidor
-const startServer = async () => {
-  await initializeDatabase();
-  
-  app.listen(PORT, () => {
-    console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
-};
 
-startServer();
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
