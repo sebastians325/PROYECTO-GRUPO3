082270ba3fcf002016bdd6b6b45279df9bc74eb0
sebasge15 <sebasge150401@gmail.com>
2025-05-18 04:17:45 -0500

    ...

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-//import Pie from '../src/ComponentesGeneral/footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +11,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
