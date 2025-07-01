75300a995b715a26726ccbf22e6169b08523dcce
eddycx <eddycx22@gmail.com>
2025-06-30 23:46:01 -0500

    dddddddd

--- servidor/routes/reviews.routes.js
+++ servidor/routes/reviews.routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const ReviewController = require('../controllers/reviews.controller');
 
 // Create instance of controller
@@ -9,12 +8,5 @@ const reviewController = new ReviewController();
 // Define routes
 router.post('/', reviewController.createReview);
 router.get('/', reviewController.getAllReviews);
-=======
-const reviewController = require('../controllers/reviews.controller');
-
-router.post('/', reviewController.createReview.bind(reviewController));
-
-router.get('/', reviewController.getAllReviews.bind(reviewController));
->>>>>>> EddyHistorialReseñas
 
 module.exports = router;
