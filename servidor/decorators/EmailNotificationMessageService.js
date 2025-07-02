5146963c0671f24f243918809b40035973ce477a
AdrianSarmiento123 <henryadriansarmiento@gmail.com>
2025-07-01 20:58:01 -0500

    Update EmailNotificationMessageService.js

--- servidor/decorators/EmailNotificationMessageService.js
+++ servidor/decorators/EmailNotificationMessageService.js
@@ -1,1 +1,25 @@
-
+class EmailNotificationMessageService {
+  constructor(baseService, emailSender, usuarioModel) {
+    this.baseService = baseService;
+    this.emailSender = emailSender;
+    this.usuarioModel = usuarioModel;
+  }
+ 
+  async createMessage(data) {
+    const message = await this.baseService.createMessage(data);
+ 
+    const destinatario = await this.usuarioModel.findByPk(data.destinatarioId);
+    if (destinatario?.correo) {
+      await this.emailSender.send(
+        destinatario.correo,
+        `Tienes un nuevo mensaje: "${data.contenido}"`
+      );
+    } else {
+      console.log(`No se encontró correo para usuario ${data.destinatarioId}`);
+    }
+ 
+    return message;
+  }
+}
+ 
+module.exports = EmailNotificationMessageService;
